1293f0cfd795a2784072a7822769116ebf8f88c6
Brahian E. Soto Mercedes <brahian@protonmail.com>
2017-04-04 20:13:14 +0200

    Returns QTI-Item as a JavaScript object.

--- src/qti2json.js
+++ src/qti2json.js
@@ -1,9 +1,20 @@
- aimport { DOMParser } from "xmldom";
+import { DOMParser } from "xmldom";
+import { getAttributes, getChildren } from "./utils";
 
-function qti2json(xmlString) {
-  const parser = new DOMParser();
-  const xmlDoc = parser.parseFromString(xmlString, "text/xml");
-  return xmlDoc;
+function qti2json(qtiString) {
+  const qtiDocument = new DOMParser().parseFromString(qtiString, "text/xml");
+  const assessmentItemAttr = qtiDocument.documentElement.attributes;
+  const assessmentItemChildren = qtiDocument.documentElement.childNodes;
+
+  const assessmentItem = {
+    ...getAttributes(["identifier", "title", "adaptive"], assessmentItemAttr),
+    children: getChildren(
+      ["itemBody", "outcomeDeclaration"],
+      assessmentItemChildren
+    )
+  };
+
+  return assessmentItem;
 }
 
 module.exports = qti2json;
